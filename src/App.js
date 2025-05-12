e19ae938070e19fa5c36b10935f81fa1832a4a1a
CHECHO2 <mijjn@naver.com>
2025-05-13 00:38:19 +0900

    ~#6

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time ");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []); // [] 빈 공백 : 딱 한번만 실행시켜 줌
+function Hello() {
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]); //keyword가 변화할 때마다 실행시켜줌
-  useEffect(() => {
-    console.log("i run only 'counter' changes.");
-  }, [counter]); //counter가 변화할 때마다 실행시켜줌
-  useEffect(() => {
-    console.log("i run when keyword & counter chang");
-  }, [keyword, counter]); //keyword 랑 counter 두 개도 적용 동시에 적용가능
+    console.log("hi :)");
+    return console.log("bye :(");
+  }, []);
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
